00ad5837800016629a7ef3e5f442abce11987609
akashto76 <akashto76@gmail.com>
2026-02-16 13:56:53 +0530

    akash update

--- user-frontend/src/app/page.tsx
+++ user-frontend/src/app/page.tsx
@@ -5,7 +5,9 @@ import BannerSlider from '@/components/BannerSlider';
 
 export default function Home() {
   return (
-    <main className="max-w-7xl mx-auto px-6 md:px-8 py-20 md:py-10 space-y-6">
+    // CHANGED: 'px-4' for tighter side margins on mobile
+    // CHANGED: 'py-4' (mobile padding) instead of 'py-20'
+    <main className="max-w-7xl mx-auto px-4 md:px-8 py-4 md:py-10 space-y-6">
       
       {/* 4-Slide Banner Section */}
       <BannerSlider />
